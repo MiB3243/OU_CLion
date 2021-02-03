e65669d095a30e718b463105a0aaeace88b4f0b1
Wiktor Szczepaniak <szcwik97@gmail.com>
2021-02-03 20:44:27 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion.dir/main.cpp.obj"
+  "OU_CLion.exe"
+  "OU_CLion.exe.manifest"
+  "OU_CLion.lib"
+  "OU_CLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
